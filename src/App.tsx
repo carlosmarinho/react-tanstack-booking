ae6cd1e02f7da5720a0401ee87668dc66aff2760
Carlos Marinho <carlos.marinho@energage.com>
2024-01-14 15:37:19 -0300

    add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <h1>Hostfully</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Hostfully</h1>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
