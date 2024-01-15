d67deb75e04c639d892f5f300f561ea54c600290
Carlos Marinho <carlos.marinho@energage.com>
2024-01-14 23:31:31 -0300

    remove json from json-server and add pending and success state

--- src/components/booking/create/CreateBooking.tsx
+++ src/components/booking/create/CreateBooking.tsx
@@ -1,56 +1,92 @@
 import { useMutation } from '@tanstack/react-query';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Spin } from 'antd';
 import { sendApiRequest } from '../../../helpers/sendApiRequest';
 import { ICreateBooking } from './ICreateBooking';
+import { useEffect } from 'react';
 
 const CreateBooking = () => {
-  const createBookingMutation = useMutation({
+  const [form] = Form.useForm();
+  const { mutate, isPending, isSuccess } = useMutation({
     mutationFn: (data: ICreateBooking) =>
       sendApiRequest(
-        'http://localhost:/3001/bookings',
+        'https://65a48cf652f07a8b4a3d73c0.mockapi.io/booking',
         'POST',
         data,
       ),
   });
 
+  console.log(
+    '\n\n***\n isSuccesss: ',
+    isSuccess,
+    '\n***\n',
+  );
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(
+        '\n\n***\n vai resetar: ',
+        isSuccess,
+        '\n***\n',
+      );
+      form.resetFields();
+    }
+  }, [isSuccess]);
+
   const onFinish = (values: ICreateBooking) => {
-    console.log('values: ', values);
-    createBookingMutation.mutate(values);
+    mutate(values);
+    if (isSuccess) {
+      form.resetFields();
+    }
   };
   return (
-    <Form name="create-booking" onFinish={onFinish}>
-      <Form.Item
-        name="location_id"
-        label="Location"
-        rules={[{ required: true }]}
+    <>
+      {isPending && (
+        <Spin tip="Loading" size="large">
+          <div className="content" />
+        </Spin>
+      )}
+      <Form
+        name="create-booking"
+        onFinish={onFinish}
+        form={form}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="customer_id"
-        label="Customer"
-        rules={[{ required: true }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="start_at"
-        label="Check-In"
-        rules={[{ required: true }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="end_at"
-        label="Check-Out"
-        rules={[{ required: true }]}
-      >
-        <Input />
-      </Form.Item>
-      <Button type="primary" htmlType="submit">
-        Reserve
-      </Button>
-    </Form>
+        <Form.Item
+          name="location_id"
+          label="Location"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="customer_id"
+          label="Customer"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="start_at"
+          label="Check-In"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="end_at"
+          label="Check-Out"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          disabled={isPending}
+        >
+          Reserve
+        </Button>
+      </Form>
+    </>
   );
 };
 
