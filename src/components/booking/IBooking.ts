b10d7e06ce8593ea65865f9dde9eb97943190317
Carlos Marinho <carlos.marinho@energage.com>
2024-01-21 23:41:46 -0300

    add create booking

---
+++ src/components/booking/IBooking.ts
@@ -0,0 +1,7 @@
+export interface Ibooking {
+  id: string;
+  locationId: string;
+  customerId: string;
+  startAt: Date;
+  endAt: Date;
+}
--- src/components/booking/list/ListBooking.tsx
+++ src/components/booking/list/ListBooking.tsx
@@ -1,5 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { sendApiRequest } from '../../../helpers/sendApiRequest';
+import { List, Spin } from 'antd';
+import { Ibooking } from '../IBooking';
+
 const ListBooking = () => {
-  return <h2>Booking List</h2>;
+  const { isLoading, data } = useQuery({
+    queryKey: ['booking'],
+    queryFn: async () => {
+      return await sendApiRequest<Ibooking[]>(
+        'https://65a48cf652f07a8b4a3d73c0.mockapi.io/booking',
+        'GET',
+      );
+    },
+  });
+
+  return (
+    <>
+      {isLoading && (
+        <Spin tip="Loading" size="large">
+          <div className="content" />
+        </Spin>
+      )}
+      <h2>Booking List</h2>
+      <List
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={item.locationId}
+              description={item.customerId}
+            />
+          </List.Item>
+        )}
+      />
+    </>
+  );
 };
 
 export default ListBooking;
