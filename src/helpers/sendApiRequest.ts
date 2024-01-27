5b51938774e7f141f6345cf8e4b75352bca5585f
Carlos Marinho <carlos.marinho@energage.com>
2024-01-27 01:30:49 -0300

    api request to get from env variable

--- src/helpers/sendApiRequest.ts
+++ src/helpers/sendApiRequest.ts
@@ -1,7 +1,8 @@
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-const BASE_URL = 'http://localhost:1337/api'
-const TOKEN = '05247f31ae3f12268093a56666056bafe80d804e9d12959e38c84afa42a159eecf18359540523f02d39dedfa42187cacc920f8dedf3bf5cce34a6e3b56401a7af804fd8a15e586526558f7e2e2259431ff333f89855e8893d0d9c819fd9a8e2026d31151411e3f0fc0f539adf9e59cb664c459c8781764bdc4613737ff3acf9b'
+const BASE_URL = process.env.REACT_APP_API_URL;
+const TOKEN = process.env.REACT_APP_API_TOKEN;
+
 
 function returnCorrectRequest(
   method: Method,
