4ca5bfe34c3de787c4ee64a11ba4ac90e4f5b6d1
Carlos Marinho <carlos.marinho@energage.com>
2024-01-27 02:38:29 -0300

    remove debug

--- src/helpers/sendApiRequest.ts
+++ src/helpers/sendApiRequest.ts
@@ -1,13 +1,7 @@
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-// const BASE_URL = process.env.REACT_APP_API_URL;
-// const TOKEN = process.env.REACT_APP_API_TOKEN;
-
-const BASE_URL = 'https://booking-strapi-api-717ba026294b.herokuapp.com/api'
-const TOKEN = '0d382e36a68c48e9ff98b460a91d14a0c076f48c7fd1b0fad96bd1735a642bd5889411ae2587ce8be2dca3f40b224f245136aaea57838b72b878b75d338e5737512c3b569200840a1b9085ef322ed70df8d915f3754b392e95eb82cb328b5074a2dceb8bd1a8c1c1e8ef79c5683c828fe583b6b9bc858b328ae6f2d19c9b3ccb'
-
-console.log('\n\n***\n baseurllll, token: ', process.env.REACT_APP_API_URL, process.env.REACT_APP_API_TOKEN, '\n***\n');
-
+const BASE_URL = process.env.REACT_APP_API_URL;
+const TOKEN = process.env.REACT_APP_API_TOKEN;
 
 function returnCorrectRequest(
   method: Method,
